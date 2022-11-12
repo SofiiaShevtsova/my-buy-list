fe47bd8d9fc530953fa166dd037f662c7c9c3f42
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-11-12 15:34:47 +0200

    add localeStoreg

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const buyList = document.querySelector(".buy-list");
 const btnCleanList = document.querySelector(".clean-list");
 
 let itemBuy = "";
+const MY_LIST = "my-list";
+
+buyList.innerHTML = localStorage.getItem(MY_LIST);
 
 function greatListItem(item) {
   const listItem = `<li class="buy-item"><p>${item}</p><button type="button" class="buy-btn">Купили</button></li>`;
@@ -23,16 +26,19 @@ function onFormSubmit(event) {
   greatListItem(itemBuy);
 
   formForBuy.reset();
+  localStorage.setItem(MY_LIST, `${buyList.innerHTML}`);
 }
 
 function onBtnCleanClick(event) {
   buyList.innerHTML = "";
+  localStorage.removeItem(MY_LIST);
 }
 
 function onBtnBuyClick(event) {
   if (event.target.classList.contains("buy-btn")) {
     console.log(event.target.previousElementSibling.classList.add("buy"));
   }
+  localStorage.setItem(MY_LIST, `${buyList.innerHTML}`);
 }
 
 formForBuy.addEventListener("submit", onFormSubmit);
