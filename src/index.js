c0fd5e37539594487728cc8d38ebca7e81aa4206
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-11-13 13:13:30 +0200

    do all from my buy list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "../node_modules/modern-normalize";
 import "./style/styles.scss";
+import { Notify } from "notiflix";
 
 const formForBuy = document.querySelector(".form");
 const inputForBuy = formForBuy.querySelector("input");
@@ -8,6 +9,7 @@ const btnCleanList = document.querySelector(".clean-list");
 
 let itemBuy = "";
 const MY_LIST = "my-list";
+const ARRAY_ITEM = "list-of-product";
 
 buyList.innerHTML = localStorage.getItem(MY_LIST);
 
@@ -18,19 +20,38 @@ function greatListItem(item) {
 
 function onFormSubmit(event) {
   event.preventDefault();
+
+  const itemArray = localStorage.getItem(ARRAY_ITEM)
+    ? localStorage.getItem(ARRAY_ITEM).split(",")
+    : [];
+  console.log(itemArray);
+
   if (inputForBuy.value.trim() === "") {
     return;
   }
+  if (itemArray.includes(inputForBuy.value)) {
+    Notify.info("Цей предмет є в списку покупок", {
+      width: "500px",
+      distance: "50px",
+      fontSize: "24px",
+    });
+    formForBuy.reset();
+
+    return;
+  }
   itemBuy = inputForBuy.value.trim();
+  itemArray.push(itemBuy);
 
   greatListItem(itemBuy);
 
   formForBuy.reset();
   localStorage.setItem(MY_LIST, `${buyList.innerHTML}`);
+  localStorage.setItem(ARRAY_ITEM, `${itemArray.join(",")}`);
 }
 
 function onBtnCleanClick(event) {
   buyList.innerHTML = "";
+  localStorage.removeItem(ARRAY_ITEM);
   localStorage.removeItem(MY_LIST);
 }
 
